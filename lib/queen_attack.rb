761d98a2d45d73f777db38e62a53d9e39e04c09a
Brian Grant and Molly McGlone <molly@>
2016-06-27 15:58:20 -0700

    add spec to check if horizontally align and fix wording on specs

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -2,6 +2,8 @@ class Array
   define_method(:queen_attack?) do |move_array|
     if self.at(0) === move_array.at(0)
       true
+    elsif self.at(1) === move_array.at(1)
+      true
     else
       false
     end
